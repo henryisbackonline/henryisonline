0c409ef099af82dfe591f51e72913bc9ed21a8e3
henryisbackonline <177583903+henryisbackonline@users.noreply.github.com>
2026-01-09 22:43:37 +1000

    modift comments

--- eleventy.config.js
+++ eleventy.config.js
@@ -99,19 +99,9 @@ export default async function(eleventyConfig) {
 	eleventyConfig.addShortcode("currentBuildDate", () => {
 		return (new Date()).toISOString();
 	});
-
-	// Features to make your build faster (when you need them)
-
-	// If your passthrough copy gets heavy and cumbersome, add this line
-	// to emulate the file copy on the dev server. Learn more:
-	// https://www.11ty.dev/docs/copy/#emulate-passthrough-copy-during-serve
-
-	// eleventyConfig.setServerPassthroughCopyBehavior("passthrough");
 };
 
 export const config = {
-	// Control which files Eleventy will process
-	// e.g.: *.md, *.njk, *.html, *.liquid
 	templateFormats: [
 		"md",
 		"njk",
@@ -120,30 +110,17 @@ export const config = {
 		"11ty.js",
 	],
 
-	// Pre-process *.md files with: (default: `liquid`)
+	// Pre-process *.md files with:
 	markdownTemplateEngine: "njk",
 
-	// Pre-process *.html files with: (default: `liquid`)
+	// Pre-process *.html files with:
 	htmlTemplateEngine: "njk",
 
-	// These are all optional:
+	// Directories to putput are:
 	dir: {
 		input: "content",          // default: "."
 		includes: "../_includes",  // default: "_includes" (`input` relative)
 		data: "../_data",          // default: "_data" (`input` relative)
 		output: "_site"
 	},
-
-	// -----------------------------------------------------------------
-	// Optional items:
-	// -----------------------------------------------------------------
-
-	// If your site deploys to a subdirectory, change `pathPrefix`.
-	// Read more: https://www.11ty.dev/docs/config/#deploy-to-a-subdirectory-with-a-path-prefix
-
-	// When paired with the HTML <base> plugin https://www.11ty.dev/docs/plugins/html-base/
-	// it will transform any absolute URLs in your HTML to include this
-	// folder name and does **not** affect where things go in the output folder.
-
-	// pathPrefix: "/",
 };
