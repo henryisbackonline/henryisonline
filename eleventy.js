8bf7b3650120da8c75a6d847622ee528e65d491e
henryisbackonline <177583903+henryisbackonline@users.noreply.github.com>
2026-01-10 13:03:26 +1000

    add named export for template formats, template engine changes, and directories

--- eleventy.js
+++ eleventy.js
@@ -1,5 +1,26 @@
 export default function (eleventyConfig) {
-	eleventyConfig.addLayoutAlias("post", "layouts/post.njk");
+    eleventyConfig.addLayoutAlias("post", "layouts/post.njk");
     eleventyConfig.addLayoutAlias("base", "layouts/base.njk");
     eleventyConfig.addLayoutAlias("home", "layouts/home.njk");
 }
+
+export const config = {
+
+    templateFormats: [
+        "md",
+        "njk",
+        "html",
+        "liquid",
+        "11ty.js",
+    ],
+
+    markdownTemplateEngine: "njk",
+    htmlTemplateEngine: "njk",
+
+    dir: {
+        input: "source",
+        includes: "../_includes",
+        data: "../_data",
+        output: "_site"
+    },
+};
