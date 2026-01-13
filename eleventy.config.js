19ed79e59b468ba587d747de07357820acd06f26
henryisbackonline <177583903+henryisbackonline@users.noreply.github.com>
2026-01-13 10:41:09 +1000

    chnage default template engines for html and markdown to nunjucks

--- eleventy.config.js
+++ eleventy.config.js
@@ -2,3 +2,8 @@ export default function(eleventyConfig) {
 	eleventyConfig.setInputDirectory('src');
 	eleventyConfig.setOutputDirectory('_site');
 }
+
+export const config = {
+	markdownTemplateEngine: 'njk',
+	htmlTemplateEngine: 'njk',
+};
