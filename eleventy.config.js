c1b3ca8371d2d243404728e426854f9704a3a398
henryisbackonline <177583903+henryisbackonline@users.noreply.github.com>
2026-01-13 11:58:58 +1000

    chnage css and js bundle output to match chose site output

--- eleventy.config.js
+++ eleventy.config.js
@@ -38,7 +38,7 @@ export default async function(eleventyConfig) {
 	// Per-page bundles, see https://github.com/11ty/eleventy-plugin-bundle
 	// Bundle <style> content and adds a {% css %} paired shortcode
 	eleventyConfig.addBundle("css", {
-		toFileDirectory: "dist",
+		toFileDirectory: "_site",
 		// Add all <style> content to `css` bundle (use <style eleventy:ignore> to opt-out)
 		// Supported selectors: https://www.npmjs.com/package/posthtml-match-helper
 		bundleHtmlContentFromSelector: "style",
@@ -46,7 +46,7 @@ export default async function(eleventyConfig) {
 
 	// Bundle <script> content and adds a {% js %} paired shortcode
 	eleventyConfig.addBundle("js", {
-		toFileDirectory: "dist",
+		toFileDirectory: "_site",
 		// Add all <script> content to the `js` bundle (use <script eleventy:ignore> to opt-out)
 		// Supported selectors: https://www.npmjs.com/package/posthtml-match-helper
 		bundleHtmlContentFromSelector: "script",
