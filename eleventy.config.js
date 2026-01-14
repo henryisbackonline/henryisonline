b74745a5e68803147c90a3d9a85d6376320d54b4
henryisbackonline <177583903+henryisbackonline@users.noreply.github.com>
2026-01-14 23:47:36 +1000

    frutless attempts at css styling, will try again in the morning

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,9 +1,27 @@
 import eleventyNavigationPlugin from "@11ty/eleventy-navigation";
+import { eleventyImageTransformPlugin } from "@11ty/eleventy-img";
 
 export default function (eleventyConfig) {
+
+    eleventyConfig.addPassthroughCopy("_assets");
+
     eleventyConfig.addPlugin(eleventyNavigationPlugin);
-    eleventyConfig.addPassthroughCopy("assets/simple.css");
+
+    eleventyConfig.addPlugin(eleventyImageTransformPlugin, {
+
+        formats: "png",
+        widths: 1200,
+        failOnError: false,
+        htmlOptions: {
+            imgAttributes: {
+                // <img loading decoding> assigned on the HTML tag will override these values.
+                loading: "lazy",
+                decoding: "async",
+            }
+        },
+    });
 };
+
 export const config = {
     markdownTemplateEngine: "njk",
     htmlTemplateEngine: "njk",
