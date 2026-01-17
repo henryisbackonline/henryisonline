feb90f520582fb5ac808f8623fb8e1715cb7e97d
henryisbackonline <177583903+henryisbackonline@users.noreply.github.com>
2026-01-17 11:30:18 +1000

    modify config to enable: - custom date plugins - image transforms

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,36 +1,45 @@
 import eleventyNavigationPlugin from "@11ty/eleventy-navigation";
 import { eleventyImageTransformPlugin } from "@11ty/eleventy-img";
 
-export default function (eleventyConfig) {
+import dateFilters from "./_config/date.js";
 
+export default function (eleventyConfig) {
 
     eleventyConfig
         .addPassthroughCopy("site-logo-800px.png")    // Copy the site logo
         .addPassthroughCopy("style.css");             // Copy the global stylesheet
 
-    // Use the eleenty navigation options
+    // Use the eleventy navigation options
     eleventyConfig.addPlugin(eleventyNavigationPlugin);
 
-    // Resize images for faster load times
     eleventyConfig.addPlugin(eleventyImageTransformPlugin, {
+        // process images from .md extensons
+        // extensions: ".md",
 
+        // transform all images to png format
         formats: "png",
-        widths: 1200,
-        failOnError: false,
+        
+        // resize all images to 1200px wide - smaller file sizes
+        widths: [1200],
+
+        // these options are from the eleventy-image docs - see below
+        // (https://www.11ty.dev/docs/plugins/image/#more-configuration-options)
         htmlOptions: {
-            imgAttributes: {
-                // <img loading decoding> assigned on the HTML tag will override these values.
+            imageAttributes: {
                 loading: "lazy",
-                decoding: "async",
+                decoding: "async"
             }
-        },
+        }
     });
-    
+
     eleventyConfig.addBundle("css", {
         toFileDirectory: "_site",
         bundleHtmlContentFromSelector: "style"
         // all of the above is from eleventy-base-blog config file
     });
+
+    // Add the cutom date filters - from eleventy-base-blog
+    eleventyConfig.addPlugin(dateFilters);
 
 };
 
@@ -41,6 +50,7 @@ export const config = {
     dir: {
         input: "_src",
         includes: "../_includes",
-        data: "../data"
+        data: "../data",
+        output: "_site"
     }
 };
